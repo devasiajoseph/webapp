aca41d0ad19f0e0b3e569eeecf6d7c24019d60da
Devasia Joseph <devasiajoseph@gmail.com>
2023-04-01 18:23:45 +0530

    table ui

--- cljs/src/aupro/profile.cljs
+++ cljs/src/aupro/profile.cljs
@@ -85,23 +85,27 @@
   (ui/render profile-form "app"))
 
 
+(def profile-list (r/atom {}))
 
 (defn profile-table 
   []
   [:div {:class "overflow-x-auto w-full"}
    [:table {:class "table w-full"}
-    [:thead [:tr [:th ""] [:th "Name"] [:th "Action"]]]
-    [:tbody [:tr 
-             [:td [:div {:class "flex items-center space-x-3"}
-                       [:div {:class "avatar"}
-                        [:div {:class "mask mask-squircle w-12 h-12"}
-                         [:img {:src "http://localhost:8081/static/uploads/images/c39b674f-72e4-4c71-b974-3f0e5bcb9376.webp", :alt "Avatar Tailwind CSS Component"}]]]]]
-             [:td "Elon musk"]
-             [:td [:button {:class "btn btn-ghost btn-xs"} "Edit"]]]]]]
-  )
+    [:thead [:tr [:th ""] [:th "Name"]]]
+    [:tbody 
+     (doall (map (fn [each]  ^{:key each}
+                   [:tr
+                    [:td [:div {:class "flex items-center space-x-3"}
+                          [:div {:class "avatar"}
+                           [:div {:class "mask mask-squircle w-12 h-12"}
+                            [:img {:src (:profile_pic each), :alt "Avatar Tailwind CSS Component"}]]]]]
+                    [:td [:a {:href (str "#/profile/edit/" (:profile_id each)) :class "btn btn-ghost btn-xs"} (:full_name each)]]])(:data @profile-list)))]]])
+
+
 
 
 (defn render-profile-list
-  [page]
-  (ui/render profile-table "app")
-  )
+  [page] 
+  (ajax/get-json (str "/api/profile?page=" page) nil 
+                 (fn[response] (reset! profile-list response)))
+  (ui/render profile-table "app"))
